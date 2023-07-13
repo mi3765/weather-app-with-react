5f9b213e83f2c139d8fb69548aa2e897ee1f31c8
mi3765 <chips1225@outlook.jp>
2023-07-13 10:15:25 +0900

    検索用タグを追加

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import "./App.css";
 import { Search } from "./components/Search/Search";
 
 const App = () => {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
   return (
     <div className="container">
-      <Search></Search>
+      <Search onSearchChange={handleOnSearchChange}></Search>
     </div>
   );
 };
